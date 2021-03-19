f7e83da361c6d8d54f0e7552a18c40cb7725b424
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-03-19 19:18:19 +0100

    initial setup, remove uncessary files, add header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import reading from "./images/reading.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="App-image" src={reading} alt="woman reading" />
       </header>
     </div>
   );
