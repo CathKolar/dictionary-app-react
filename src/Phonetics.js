9fe5db7e264c2562eddee8188dcfd67e28d0ef1e
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-03-29 11:32:36 +0200

    create/render  Phonetics.js, console.log API response to explore the possibilities of this component

---
+++ src/Phonetics.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Phonetics({ phonetics }) {
+  console.log({ phonetics });
+  return (
+    <div className="Phonetics">
+      <h2>Hello from Phonetics</h2>
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Phonetics from "./Phonetics";
 import Meaning from "./Meaning";
 
 export default function Results({ results }) {
@@ -7,6 +8,13 @@ export default function Results({ results }) {
     return (
       <div className="Results">
         <h2>{results.word}</h2>
+        {results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         {results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
