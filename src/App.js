8ae0f54b0098e7f1252698f6c2fa709f96c450f2
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-03-30 12:29:49 +0200

    add contact to and style footer, add background color to body and sections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import reading from "./images/reading.png";
+import reading1 from "./images/reading1.png";
 import Dictionary from "./Dictionary";
 import "./App.css";
 
@@ -6,13 +6,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-image" src={reading} alt="woman reading" />
+        <img className="App-image" src={reading1} alt="woman reading" />
       </header>
       <main>
         <Dictionary />
       </main>
+
       <footer className="App-footer">
-        <small>Coded by Catherine Kolar</small>
+        <small>
+          <a
+            href="https://github.com/CathKolar/dictionary-app-react"
+            target="blank"
+            title="GitHub"
+          >
+            <span class="link"> Open-source code </span>
+          </a>
+          by
+          <a
+            href="https://www.linkedin.com/in/catherinekolar/"
+            target="blank"
+            title="LinkedIn"
+          >
+            <span class="link"> Catherine Kolar </span>
+          </a>
+        </small>
       </footer>
     </div>
   );
