701678e0a576804ac3f55f243cab29babebd0455
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-03-30 19:56:31 +0200

    style photos, update alt text,clean up class names, edit pexels api call

--- src/Photos.js
+++ src/Photos.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Photos.css";
 
-export default function Photos({ photos, results }) {
+export default function Photos({ photos }) {
   if (photos) {
     return (
-      <section className="Photos photo-row">
+      <section className="Photos">
         {photos.map(function (photo) {
           return (
-            <div className="photo-column">
-              <img src={photo.src.original} key={photo.id} alt={results.word} />
+            <div>
+              <a href={photo.src.original} target="blank">
+                <img
+                  src={photo.src.landscape}
+                  key={photo.id}
+                  alt={photo.src.photographer}
+                />
+              </a>
             </div>
           );
         })}
